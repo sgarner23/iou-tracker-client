a1a2b0faa193f9f2c8db3c4c8e36d90c2c15373b
Stephen Garner <stephenallen.garner@gmail.com>
2021-12-09 12:30:58 -0700

    still working on deployment

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
